100c56b291bd61a1ae401d73fed1646c8c1b7da5
Kristof degrave <kristof.degrave@telenet.be>
2017-04-17 15:16:24 +0200

    Passed versions on the correct way

--- src/Database.js
+++ src/Database.js
@@ -74,7 +74,7 @@ define('Database', [
 
         function upgradeInternal(db, openDBRequest, currentVersion, version, connection, successCallback, failCallback) {
             if(db.connections.length > 0 && db.connections[0].__connectionId !== connection.__connectionId){
-                openDBRequest.__blocked(currentVersion, connection.version);
+                openDBRequest.__blocked(connection.version, currentVersion);
                 setTimeout(upgradeInternal, 10, db, openDBRequest, currentVersion, version, connection, successCallback, failCallback);
                 return;
             }
